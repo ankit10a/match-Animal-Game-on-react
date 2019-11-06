9fc8b5b8c04860f1fa64256b615517f77cf4b733
ankit10a <42811565+ankit10a@users.noreply.github.com>
2019-11-06 11:41:21 +0530

    Add files via upload

--- maincomponet.jsx
+++ maincomponet.jsx
@@ -1,266 +1,91 @@
 import React, { Component } from "react";
-import Cell from "./cell";
+import { getQuestions } from "./getQuestion";
+import Question from "./question";
+import ViewMarksheet from "./marksheet";
 
-class Grid extends Component {
+class QuizApp extends Component {
   state = {
-    cellvalue: [
-      {
-        img:
-          "http://pluspng.com/img-png/free-funny-animated-png-hd-courtesy-clipart-funny-animation-4-1000.png",
-        present: false,
-        name: "bat.png",
-        open: false
-      },
-      {
-        img:
-          "http://pluspng.com/img-png/free-funny-animated-png-hd-animated-dancing-turkey-clipart-download-1096.png",
-        present: false,
-        name: "chicken.png",
-        open: false
-      },
-      {
-        img:
-          "http://pluspng.com/img-png/free-funny-animated-png-hd-courtesy-clipart-funny-animation-4-1000.png",
-        present: false,
-        name: "bat.png",
-        open: false
-      },
-      {
-        img:
-          "http://pluspng.com/img-png/free-funny-animated-png-hd-you-re-welcome-clip-art-free-939.png",
-        present: false,
-        name: "bee.png",
-        open: false
-      },
-      {
-        img:
-          "http://pluspng.com/img-png/free-funny-animated-png-hd-cute-cartoon-animals-pictures-314.png",
-        present: false,
-        name: "penguine.png",
-        open: false
-      },
-      {
-        img:
-          "http://pluspng.com/img-png/free-funny-animated-png-hd-animated-dancing-turkey-clipart-download-1096.png",
-        present: false,
-        name: "chicken.png",
-        open: false
-      },
-      {
-        img: "http://pluspng.com/img-png/free-funny-animated-png-hd--426.png",
-        present: false,
-        name: "squerel.png",
-        open: false
-      },
-      {
-        img:
-          "http://pluspng.com/img-png/free-funny-animated-png-hd-you-re-welcome-clip-art-free-939.png",
-        present: false,
-        name: "bee.png",
-        open: false
-      },
-      {
-        img:
-          "http://pluspng.com/img-png/free-funny-animated-png-hd-cow-cartoon-funny-cute-dancing-isolated-644.png",
-        present: false,
-        name: "cow.png",
-        open: false
-      },
-      {
-        img:
-          "http://pluspng.com/img-png/free-funny-animated-png-hd-year-of-the-monkey-clipart-tree-png-13-600.jpg",
-        present: false,
-        name: "monkey.jpg",
-        open: false
-      },
-      {
-        img:
-          "http://pluspng.com/img-png/free-funny-animated-png-hd-funny-cartoon-cows-700.png",
-        present: false,
-        name: "owl.png",
-        open: false
-      },
-      {
-        img:
-          "http://pluspng.com/img-png/free-funny-animated-png-hd-cow-cartoon-funny-cute-dancing-isolated-644.png",
-        present: false,
-        name: "cow.png",
-        open: false
-      },
-      {
-        img:
-          "http://pluspng.com/img-png/free-funny-animated-png-hd-cute-cartoon-animals-pictures-314.png",
-        present: false,
-        name: "penguine.png",
-        open: false
-      },
-      {
-        img:
-          "http://pluspng.com/img-png/free-funny-animated-png-hd-year-of-the-monkey-clipart-tree-png-13-600.jpg",
-        present: false,
-        name: "monkey.jpg",
-        open: false
-      },
-      {
-        img:
-          "http://pluspng.com/img-png/free-funny-animated-png-hd-funny-cartoon-cows-700.png",
-        present: false,
-        name: "owl.png",
-        open: false
-      },
-      {
-        img: "http://pluspng.com/img-png/free-funny-animated-png-hd--426.png",
-        present: false,
-        name: "squerel.png",
-        open: false
-      }
-    ],
-    open1: -1,
-    open2: -1,
-    count: 0,
-    reset: []
+    questions: getQuestions(),
+    currentIndex: 0,
+    response: [],
+    view: 0
   };
-  handleclick = ind => {
-    if (this.state.open2 === -1) {
-      let cellvalue = { ...this.state.cellvalue };
-      cellvalue[ind].open = true;
-      if (this.state.open1 != -1) this.state.open2 = ind;
-      if (this.state.open1 === -1) this.state.open1 = ind;
-      if (this.state.open1 != -1 && this.state.open2 != -1) {
-        let result = setTimeout(
-          () => this.doCellsMatching(this.state.open1, this.state.open2),
-          1000
-        );
-      }
-      this.setState({ cellvalue });
-    }
+  IntialiseResponse = () => {
+    let response = this.state.questions.map(n => ({
+      ques: n.text,
+      ans: ""
+    }));
+    this.setState({ response });
   };
-  doCellsMatching = (op1, op2) => {
-    if (this.state.cellvalue[op1].name === this.state.cellvalue[op2].name) {
-      this.state.cellvalue[op1].present = true;
-      this.state.cellvalue[op2].present = true;
-      this.state.count += 1;
-    }
-    this.state.cellvalue[op1].open = false;
-    this.state.cellvalue[op2].open = false;
-    this.setState({ open1: -1, open2: -1 });
+  handleNext = () => {
+    if (this.state.currentIndex < 14)
+      this.setState({
+        currentIndex: this.state.currentIndex + 1
+      });
   };
-  handleReset = () => {
-    this.setState({
-      open1: -1,
-      open2: -1,
-      count: 0
-    });
-    let cellvalue = [...this.state.reset];
-    let obj = cellvalue.map((ele, index) => {
-      this.state.cellvalue[index].open = false;
-      this.state.cellvalue[index].present = false;
-    });
+  handlePrevious = () => {
+    if (this.state.currentIndex > 0)
+      this.setState({ currentIndex: this.state.currentIndex - 1 });
   };
-  intialisereset = () => {
-    if (this.state.reset.length === 0) {
-      this.state.reset = [...this.state.cellvalue];
-    }
+  handleResponse = ans => {
+    let response = [...this.state.response];
+    response[this.state.currentIndex].ans = ans;
+    this.setState({ response });
+  };
+  handleView = () => {
+    this.setState({ view: 1 });
   };
   render() {
     return (
       <div className="container">
-        {this.intialisereset()}
-        <h3>{this.state.count === 8 ? "Game Over" : ""}</h3>
-        <div className="row">
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[0]}
-            index={0}
-          ></Cell>
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[1]}
-            index={1}
-          ></Cell>
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[2]}
-            index={2}
-          ></Cell>
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[3]}
-            index={3}
-          ></Cell>
+        {this.state.response.length === 0 ? this.IntialiseResponse() : ""}
+        <div className="bg-light">
+          <h1 className="text-center">General Knowledge : Assignment 4A</h1>
+          <div className="row">
+            <div className="col-2 m-2"> Time : 5mins</div>
+            <div className="col-5" />
+            <div className="col-4 text-right m-1">
+              Max Score:{this.state.questions.length}
+            </div>
+          </div>
         </div>
-        <div className="row">
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[4]}
-            index={4}
-          ></Cell>
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[5]}
-            index={5}
-          ></Cell>
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[6]}
-            index={6}
-          ></Cell>
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[7]}
-            index={7}
-          ></Cell>
+        <div className="col-12 text-right">
+          <button className="btn btn-primary" onClick={() => this.handleView()}>
+            View Marking Sheet
+          </button>
         </div>
-        <div className="row">
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[8]}
-            index={8}
-          ></Cell>
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[9]}
-            index={9}
-          ></Cell>
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[10]}
-            index={10}
-          ></Cell>
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[11]}
-            index={11}
-          ></Cell>
-        </div>
-        <div className="row">
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[12]}
-            index={12}
-          ></Cell>
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[13]}
-            index={13}
-          ></Cell>
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[14]}
-            index={14}
-          ></Cell>
-          <Cell
-            handleclick={this.handleclick}
-            cellvalue={this.state.cellvalue[15]}
-            index={15}
-          ></Cell>
-        </div>
-        <button className="btn btn-primary" onClick={() => this.handleReset()}>
-          {this.state.count === 8 ? "New Game" : "Reset"}
-        </button>
+        {this.state.view === 0 ? (
+          <div>
+            <Question
+              question={this.state.questions}
+              currentIndex={this.state.currentIndex}
+              handleResponse={this.handleResponse}
+              choosenOption={this.state.choosenOption}
+              response={this.state.response}
+            />
+            {this.state.currentIndex > 0 && (
+              <button
+                className="btn btn-primary m-2"
+                onClick={() => this.handlePrevious()}
+              >
+                Previous Question
+              </button>
+            )}
+            <button
+              className="btn btn-primary m-2"
+              onClick={() => this.handleNext()}
+            >
+              Next Question
+            </button>
+          </div>
+        ) : (
+          <div>
+            <ViewMarksheet response={this.state.response} />
+          </div>
+        )}
       </div>
     );
   }
 }
 
-export default Grid;
+export default QuizApp;
